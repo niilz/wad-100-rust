f73d19e2db3a00af58572bc45fb5e720bc3e09f3
niilz <niilz@outlook.de>
2023-07-23 05:52:12 -0400

    add demo for custom claims

--- src/jwt.rs
+++ src/jwt.rs
@@ -1,22 +1,56 @@
 use jwt_simple::{algorithms::{HS256Key, MACLike}, claims::{JWTClaims, NoCustomClaims}, token::Token};
+use serde::{{Serialize, Deserialize}, de::DeserializeOwned};
 
 pub struct JwtHelper {
+}
 
+#[derive(Serialize, Deserialize, Debug)]
+pub struct DummyClaims {
+    pub a: usize,
+    pub b: usize,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ClaimWrapper {
+    pub custom: DummyClaims,
 }
 
 impl JwtHelper {
-    pub fn decode_token(jwt: &str) -> JWTClaims<NoCustomClaims> {
+    pub fn decode_token_no_custom(jwt: &str) -> JWTClaims<NoCustomClaims> {
+        print_token_details(jwt);
         
+        extract_verification_result_no_custom(jwt, "your-256-bit-secret")
+    }
+
+    pub fn decode_token(jwt: &str) -> JWTClaims<ClaimWrapper> {
+        print_token_details(jwt);
+        
+        extract_verification_result(jwt, "your-256-bit-secret")
+    }
+}
+fn print_token_details(jwt: &str) {
         let token = Token::decode_metadata(jwt).unwrap();
         println!("{token:?}");
         let alg = token.algorithm();
         println!("{alg}");
-        let key = HS256Key::from_bytes(b"your-256-bit-secret");
+}
+
+fn extract_verification_result_no_custom(jwt: &str, key: &str) -> JWTClaims<NoCustomClaims> {
+        let key = HS256Key::from_bytes(key.as_bytes());
         println!("{key:?}");
         let verification_result = key.verify_token::<NoCustomClaims>(jwt, None);
         println!("{verification_result:?}");
         let verification = verification_result.unwrap();
-        println!("{verfication:?}");
+        println!("{verification:?}");
         verification
-    }
+}
+
+fn extract_verification_result(jwt: &str, key: &str) -> JWTClaims<ClaimWrapper> {
+        let key = HS256Key::from_bytes(key.as_bytes());
+        println!("{key:?}");
+        let verification_result = key.verify_token::<ClaimWrapper>(jwt, None);
+        println!("{verification_result:?}");
+        let verification = verification_result.unwrap();
+        println!("{verification:?}");
+        verification
 }
--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,12 @@ use wad_100_rust::answer::AnswerService;
 use wad_100_rust::jwt::JwtHelper;
 
 const JWT: &str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+const JWT_CLAIMS: &str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyLCJjdXN0b20iOnsiYSI6MSwiYiI6Mn19.ZgdjGSJStnfDs37qHIiLIXZE9VMRToYyRyyn-M0KeqI";
 
 #[tokio::main]
 async fn main() {
-    let claims = JwtHelper::decode_token(JWT);
+    let claims = JwtHelper::decode_token_no_custom(JWT);
+    let claims = JwtHelper::decode_token(JWT_CLAIMS);
 
     let answer_service = AnswerService::new();
     let response = answer_service.submit("the answer").await;
